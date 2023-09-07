a3e6e30239e13ec37bdb5de512332fca27f7833f
Pavlen <pavlen@192.168.1.77>
2023-09-07 18:54:08 +0300

    fron/Dockerfile and Docker volume

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,12 +59,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Docker volume ретранслятор
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
-}
+} 
 
 
 # Password validation
